04cd4ab20da1ac1f62aaca6ef5868d9f29068831
edwaldkeeley <edwaldpk@gmail.com>
2022-07-05 12:39:45 +0700

    first commit

--- src/js/config.js
+++ src/js/config.js
@@ -2,5 +2,5 @@
 export const API_URL2 = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC2 = 5;
 export const RES_PER_PAGE2 = 10;
-export const KEY2 = 'YOUR OWN KEY';
+export const KEY2 = 'YOUR KEY';
 export const MODAL_CLOSE_SEC2 = 1.6;
