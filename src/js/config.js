43cd21add66577f09897f0d57b3b492d84466640
edwaldkeeley <edwaldpk@gmail.com>
2022-07-07 19:04:15 +0700

    Fixed Some bugs

--- src/js/config.js
+++ src/js/config.js
@@ -2,5 +2,5 @@
 export const API_URL2 = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC2 = 5;
 export const RES_PER_PAGE2 = 10;
-export const KEY2 = 'YOUR KEY';
+export const KEY2 = '7694a56b-81d5-459c-beec-9dbaceeb552a';
 export const MODAL_CLOSE_SEC2 = 1.6;
